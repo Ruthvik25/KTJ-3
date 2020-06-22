45f200fcc21528b985a6674d5c1b92b986b38dcd
Ruthvik25 <ruthvikpawar25@gmail.com>
2020-06-22 21:23:41 +0530

    Updates

---
+++ precache-manifest.100024e8ca3c11a1a40d760d2c26dcbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709912b630191c61054a8a948c9d429d",
+    "url": "/KTJ-3/index.html"
+  },
+  {
+    "revision": "277ccb2d62dfef6c96f7",
+    "url": "/KTJ-3/static/css/main.2a98b4e4.chunk.css"
+  },
+  {
+    "revision": "e51b645159f3e456fdcb",
+    "url": "/KTJ-3/static/js/2.97576b62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KTJ-3/static/js/2.97576b62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "277ccb2d62dfef6c96f7",
+    "url": "/KTJ-3/static/js/main.4edf441c.chunk.js"
+  },
+  {
+    "revision": "6c115e2c37c18b61ced0",
+    "url": "/KTJ-3/static/js/runtime-main.55ec3d0f.js"
+  }
+]);
